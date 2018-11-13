3582b536f42309ea28c04e5e4f612b0fd4ca7e20
Maithili Kalamkar <maithili86@gmail.com>
2018-11-13 19:23:37 +0100

    Create python2.py

---
+++ python2.py
@@ -0,0 +1,24 @@
+# Script to print Date and Time
+
+from datetime import datetime
+
+now = datetime.now()
+
+mm = str(now.month)
+
+dd = str(now.day)
+
+yyyy = str(now.year)
+
+hour = str(now.hour)
+
+mi = str(now.minute)
+
+ss = str(now.second)
+
+print "====================================================================="
+print "Hello, you are running the Python2 script to print the date and time" 
+print "====================================================================="
+
+print "Date: " + mm + "/" + dd + "/" + yyyy + " Time: " + hour + ":" + mi + ":" + ss
+
