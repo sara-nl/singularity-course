b546ea984910fe2e5c45f6265b34a4b26fafe8f8
oonkjbr <oonkjbr@gmail.com>
2018-11-14 11:09:11 +0100

    Update jobsubmit.sh

--- jobsubmit.sh
+++ jobsubmit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -t 10:00
-echo "Hello I am running a singularity job"
+echo "Hello I am running a Singularity job"
 singularity --version
 echo "I am here" $PWD
-echo "Running the job with singularity now"
+echo "Running the job with Singularity now"
 singularity exec python-2-7.simg python python2.py
 singularity exec python-3.simg python python2.py
 singularity exec python-3.simg python python3.py
